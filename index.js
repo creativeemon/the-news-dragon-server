4f2cb238065bc94ba6c6114fe74b169459a93aad
MD:sahariar Ahammed Emon <sahariargd28@gmail.com>
2023-05-02 09:49:19 +0600

    verscel config file added

--- index.js
+++ index.js
@@ -6,13 +6,36 @@ const port = 3000
 app.use(cors())
 
 const categories = require('./Data/categories.json')
+const news = require('./Data/news.json');
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Dragon is running!!!!')
   })
    
   app.get('/categories', (req , res) => {
     res.send(categories)
+  })
+
+  app.get('/news', (req, res) => {
+    res.send(news);
+  })
+
+  app.get('/news/:id', (req , res) => {
+    const id =req.params.id;
+    console.log(id)
+    const selectedNews = news.find(n=> n._id ===id);
+    res.send(selectedNews)
+  })
+
+  app.get('/categories/:id', (req , res) =>{
+    const id =parseInt(req.params.id);
+    if(id === 0){
+      res.send(news)
+    }
+    else{
+      const categories =news.filter(n=> parseInt(n.category_id) ===id);
+      res.send(categories)
+    }
   })
 
   app.listen(port, () => {
